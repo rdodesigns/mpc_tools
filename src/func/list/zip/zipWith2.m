d22cb1616496c39989582f9a29bc73a2496ea374
Ryan Orendorff <ryan@rdodesigns.com>
2013-12-06 19:24:51 -0800

    Help comments for zipWith2.

--- src/func/list/zip/zipWith2.m
+++ src/func/list/zip/zipWith2.m
@@ -1,3 +1,19 @@
+%ZIPWITH2: Zip two lists together is a binary function f.
+%
+%   list = zipWith2(f, a, b) takes two lists and combines them into a new list,
+%       where each element of the new list is the result of the function on the
+%       head of the input a and b lists. This is similar to a fold, but in this
+%       case using two lists as an input.
+%
+%   This function can be partially applied. If only one argument is supplied (a
+%   function f), then the result is a function that takes two lists and applies
+%   the function f to the elements of the two lists. Similarly, filling in two
+%   arguments (a function f and a list a) returns a function that takes in a
+%   list and applies the function f to the originally supplied a and the input
+%   list.
+%
+%   See also CONCATMAP
+
 function out = zipWith2(f, a, b)
 
   switch nargin
